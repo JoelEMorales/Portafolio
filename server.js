ee96fd6a3df1a16ff05843883e04f9b82977c1bd
Joel Morales <joelelianmorales@gmail.com>
2024-08-30 16:53:38 +0200

    Modifico server.js para servir archivos estáticos

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-// server.js
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 
 // Cargar variables de entorno desde el archivo .env
 dotenv.config();
 
 const app = express();
 app.use(cors());  // Permitir que el frontend se comunique con el backend
+
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta para enviar las credenciales de EmailJS
 app.get('/emailjs-credentials', (req, res) => {
@@ -16,6 +19,11 @@ app.get('/emailjs-credentials', (req, res) => {
         templateID: process.env.EMAILJS_TEMPLATE_ID,
         publicKey: process.env.EMAILJS_PUBLIC_KEY
     });
+});
+
+// Ruta para manejar todas las demás peticiones y servir el archivo index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Iniciar el servidor en el puerto 3000
